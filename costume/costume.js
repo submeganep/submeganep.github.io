102609e727de0bd6e58f06f745014619bc66fbd9
submeganep <64298377+submeganep@users.noreply.github.com>
2020-05-25 02:09:12 +0900

    Add files via upload

--- costume/costume.js
+++ costume/costume.js
@@ -1,5 +1,95 @@
 $.getJSON('https://raw.githubusercontent.com/submeganep/submeganep.github.io/master/costume/costume.json', function (data) {
     let keys = Object.keys(data);
+    let indices = [];
+
+    // 代表色のカラーコードをL*a*b*に変換
+    for (let key of keys) {
+        let lab = [];
+        for (let color of data[key]['color']) {
+            lab.push(convertToLab(color));
+        }
+        data[key]['lab'] = lab;
+    }
+
+    // カラーピッカー
+    $("#picker").spectrum({
+        preferredFormat: "hex",
+        showInput: true,
+        showInitial: true,
+        allowEmpty: true,
+        color: '',
+        change: function(color) {
+            if (color != null) {
+                displayColorCostumes(color.toHexString());
+            }
+        },
+    });
+
+    // 更新
+    $('input[type="checkbox"]').change(function() {
+        displayCostumes();
+    });
+    $('input[type="date"]').change(function() {
+        displayCostumes();
+    });
+    $('input[type="number"]').change(function() {
+        displayCostumes();
+    });
+
+    // ボタン
+    $('#button_ticket').on('click', function() {
+        $('#type_const').prop('checked', true);
+        $('#type_limit').prop('checked', false);
+        $('#type_fes').prop('checked', false);
+        $('#type_pst').prop('checked', false);
+        $('#type_sr').prop('checked', false);
+        $('#date_begin').val('2017-06-29');
+        $('#date_end').val('2020-04-19');
+        displayCostumes();
+    });
+    $('#button_psp').on('click', function() {
+        $('#type_const').prop('checked', false);
+        $('#type_limit').prop('checked', false);
+        $('#type_fes').prop('checked', false);
+        $('#type_pst').prop('checked', true);
+        $('#type_sr').prop('checked', false);
+        $('#date_begin').val('2017-06-29');
+        $('#date_end').val('2019-10-20');
+        displayCostumes();
+    });
+    $('#button_all').on('click', function() {
+        $('#type_const').prop('checked', true);
+        $('#type_limit').prop('checked', true);
+        $('#type_fes').prop('checked', true);
+        $('#type_pst').prop('checked', true);
+        $('#type_sr').prop('checked', true);
+        $('#date_begin').val('2017-06-29');
+        $('#date_end').val('');
+        displayCostumes();
+    });
+
+    // アイドル選択
+    const colors = ['gray', '#ff2284', '#005eff', '#ffbb00'];
+    const as_list = ['天海春香', '如月千早', '星井美希', '萩原雪歩', '高槻やよい', '菊地真', '水瀬伊織', '四条貴音', '秋月律子', '三浦あずさ', '双海亜美', '双海真美', '我那覇響'];
+    const pr_list = ['春日未来', '田中琴葉', '佐竹美奈子', '徳川まつり', '七尾百合子', '高山紗代子', '松田亜利沙', '高坂海美', '中谷育', 'エミリー', '矢吹可奈', '横山奈緒', '福田のり子'];
+    const fa_list = ['最上静香', '所恵美', 'ロコ', '天空橋朋花', '北沢志保', '舞浜歩', '二階堂千鶴', '真壁瑞希', '百瀬莉緒', '永吉昴', '周防桃子', 'ジュリア', '白石紬'];
+    const an_list = ['伊吹翼', '島原エレナ', '箱崎星梨花', '野々原茜', '望月杏奈', '木下ひなた', '馬場このみ', '大神環', '豊川風花', '宮尾美也', '篠宮可憐', '北上麗花', '桜守歌織'];
+    const names_list = [as_list, pr_list, fa_list, an_list];
+    for (let i = 0; i < 4; i++) {
+        let names = names_list[i];
+        let group = $('<div class="idol_group" style="border-left: solid 10px ' + colors[i] + ';"></div>');
+        for (let name of names) {
+            let icon_img = 'https://raw.githubusercontent.com/submeganep/submeganep.github.io/master/icon/2nd/' + name + '.png';
+            let icon = $('<div class="idol_icon" id="' + name + '"><a><img class="icon_image" src="' + icon_img + '" title="' + name + '"></a></div>');
+            group.append(icon);
+        }
+        $('#idol_container').append(group);
+        for (let name of names) {
+            $('#' + name).on('click', function() {
+                displayIdolCostumes(name);
+            });
+        }
+    }
 
     function displayCostume(key) {
         const costume_img = 'https://raw.githubusercontent.com/submeganep/submeganep.github.io/master/costume/original/' + key + '.png';
@@ -16,13 +106,14 @@ $.getJSON('https://raw.githubusercontent.com/submeganep/submeganep.github.io/mas
         }
 
         let colors = $('<div class="costume_colors"></div>');
-        for (let color of data[key]['mean']) {
+        for (let color of data[key]['color']) {
             colors.append('<a><div class="costume_color" style="background-color: ' + color + ';" id="' + color.slice(1) + '"></div></a>');
         }
 
         let item = $('<div class="costume_item"></div>');
         item.append('<span class="costume_name">' + data[key]['name'] + '</span>');
         item.append(names);
+        item.append('<span class="costume_type">' + data[key]['type'] + ' / ' + data[key]['datetime'].split('T')[0] + '</span>');
         item.append('<div class="costume_image" id="' + key + '"><a><img src="' + costume_img + '"></a></div>');
         item.append(colors);
         let description = data[key.split('_v')[0]]['description'].replace(/\r?\n/g, '<br>');
@@ -30,58 +121,109 @@ $.getJSON('https://raw.githubusercontent.com/submeganep/submeganep.github.io/mas
         let description_hover = data[key]['description'].replace(/\r?\n/g, '<br>');
         $('#costume_container').append(item);
         $('#' + key).on('click', function() {
-            $('#picker').spectrum('set', '');
-            $('html, body').animate({scrollTop:$('#costume_container').offset().top});
-            $('#costume_container').empty();
             displaySimilarCostumes(key);
         });
         for (let name of data[key]['idol']) {
             $('#' + name).on('click', function() {
-                $('#picker').spectrum('set', '');
-                $('html, body').animate({scrollTop:$('#costume_container').offset().top});
-                $('#costume_container').empty();
                 displayIdolCostumes(name);
             });
         }
-        for (let color of data[key]['mean']) {
+        for (let color of data[key]['color']) {
             $('#' + color.slice(1)).on('click', function() {
-                $('#picker').spectrum('set', color);
-                $('html, body').animate({scrollTop:$('#input_container').offset().top});
-                $('#costume_container').empty();
                 displayColorCostumes(color);
             });
         }
     }
     
-    function displaySimilarCostumes(key) {
-        for (let i of data[key]['sort']) {
+    function displayCostumes() {
+        // 表示を初期化
+        $('#costume_container').empty();
+
+        const max_costumes = $('#max_costumes').val();
+        let num_costumes = 0;
+        for (let i of indices) {
+            
+            // 種類で絞込
+            const type = data[keys[i]]['type'];
+            if (type == '恒常SSR') {
+                if (!$('#type_const').prop('checked')) {
+                    continue;
+                }
+            }
+            else if (type == '限定SSR') {
+                if (!$('#type_limit').prop('checked')) {
+                    continue;
+                }
+            }
+            else if (type == 'FES') {
+                if (!$('#type_fes').prop('checked')) {
+                    continue;
+                }
+            }
+            else if (type == 'PST') {
+                if (!$('#type_pst').prop('checked')) {
+                    continue;
+                }
+            }
+            else if (type == 'SR') {
+                if (!$('#type_sr').prop('checked')) {
+                    continue;
+                }
+            }
+            else {  // 周年衣装などはスキップ
+                continue;
+            }
+
+            // 期間で絞込
+            const date = Date.parse(data[keys[i]]['datetime'].split('T')[0]);
+            const date_begin = Date.parse($('#date_begin').val());
+            const date_end = Date.parse($('#date_end').val());
+            if (!isNaN(date_begin)) {
+                if (date < date_begin) {
+                    continue;
+                }
+            }
+            if (!isNaN(date_end)) {
+                if (date > date_end) {
+                    continue;
+                }
+            }
+
+            // 表示
             displayCostume(keys[i]);
-        };
+
+            // 最大表示数
+            num_costumes++;
+            if (num_costumes >= max_costumes) {
+                break;
+            }
+        }
     }
 
     function displayIdolCostumes(name) {
-        let keys_idol = keys.filter(function(key) {
-            let cond = data[key]['idol'].includes(name);
-            cond = cond && !data[key]['id'].startsWith('ex');
-            // cond = cond && !key.startsWith('srufs');
-            return cond;
-        });
-        keys_idol.sort();
-        for (let key of keys_idol) {
-            displayCostume(key);
-        };
+        indices = [];
+        for (let i = 0; i < keys.length; ++i) {
+            if (data[keys[i]]['idol'].includes(name)) {
+                indices.push(i);
+            }
+        }
+        displayCostumes();
+        $('#picker').spectrum('set', '');
+        $('html, body').animate({scrollTop:$('#costume_container').offset().top});
     }
     
-    function displayColorCostumes(color, num_costumes=12) {
+    function displaySimilarCostumes(key) {
+        indices = data[key][$('#sort_mode').val()]
+        displayCostumes();
+        $('#picker').spectrum('set', '');
+        $('html, body').animate({scrollTop:$('#costume_container').offset().top});
+    }
+    
+    function displayColorCostumes(color) {
         let lab = convertToLab(color);
         let min_dists = [];
         for (let key of keys) {
             let dists = [];
-            // for (let color_ of data[key]['mean']) {
-            //     let lab_ = convertToLab(color_);
-            //     let dist = (lab[0] - lab_[0])**2 + (lab[1] - lab_[1])**2 + (lab[2] - lab_[2])**2;
-            //     dists.push(dist);
-            // }
             for (let lab_ of data[key]['lab']) {
                 let dist = (lab[0] - lab_[0])**2 + (lab[1] - lab_[1])**2 + (lab[2] - lab_[2])**2;
                 dists.push(dist);
@@ -91,13 +233,13 @@ $.getJSON('https://raw.githubusercontent.com/submeganep/submeganep.github.io/mas
 
         // argsort
         let len = min_dists.length;
-        let indices = new Array(len);
+        indices = new Array(len);
         for (let i = 0; i < len; ++i) indices[i] = i;
         indices.sort(function (a, b) { return min_dists[a] < min_dists[b] ? -1 : min_dists[a] > min_dists[b] ? 1 : 0; });
 
-        for (let i of indices.slice(0, num_costumes)) {
-            displayCostume(keys[i]);
-        }
+        displayCostumes();
+        $('#picker').spectrum('set', color);
+        $('html, body').animate({scrollTop:$('#query_container').offset().top});
     }
     
     function convertToLab(color) {
@@ -119,59 +261,5 @@ $.getJSON('https://raw.githubusercontent.com/submeganep/submeganep.github.io/mas
         z = z > 0.008856 ? Math.pow(z, 1 / 3) : (7.787 * z) + (4 / 29);
         return [(116 * y) - 16, 500 * (x - y), 200 * (y - z)];
     }
-
-    for (let key of keys) {
-        let lab = [];
-        for (let color of data[key]['mean']) {
-            lab.push(convertToLab(color));
-        }
-        data[key]['lab'] = lab;
-    }
-
-    $("#picker").spectrum({
-        preferredFormat: "hex",
-        showInput: true,
-        showInitial: true,
-        allowEmpty: true,
-        color: '',
-        // showPalette: true,
-        // palette: [
-        //     ['#e22b30', '#2743d2', '#b4e04b', '#d3dde9', '#f39939', '#515558', '#fd99e1', '#a6126a', '#01a860', '#9238be', '#ffe43f', '#ffe43f', '#01adb9'],
-        // ],
-        change: function(color) {
-            if (color != null) {
-                $("html, body").animate({scrollTop:$('#input_container').offset().top});
-                $('#costume_container').empty();
-                displayColorCostumes(color.toHexString());
-            }
-        },
-    });
-
-    const colors = ['gray', '#ff2284', '#005eff', '#ffbb00'];
-    const as_list = ['天海春香', '如月千早', '星井美希', '萩原雪歩', '高槻やよい', '菊地真', '水瀬伊織', '四条貴音', '秋月律子', '三浦あずさ', '双海亜美', '双海真美', '我那覇響'];
-    const pr_list = ['春日未来', '田中琴葉', '佐竹美奈子', '徳川まつり', '七尾百合子', '高山紗代子', '松田亜利沙', '高坂海美', '中谷育', 'エミリー', '矢吹可奈', '横山奈緒', '福田のり子'];
-    const fa_list = ['最上静香', '所恵美', 'ロコ', '天空橋朋花', '北沢志保', '舞浜歩', '二階堂千鶴', '真壁瑞希', '百瀬莉緒', '永吉昴', '周防桃子', 'ジュリア', '白石紬'];
-    const an_list = ['伊吹翼', '島原エレナ', '箱崎星梨花', '野々原茜', '望月杏奈', '木下ひなた', '馬場このみ', '大神環', '豊川風花', '宮尾美也', '篠宮可憐', '北上麗花', '桜守歌織'];
-    const names_list = [as_list, pr_list, fa_list, an_list];
-    for (let i = 0; i < 4; i++) {
-        let names = names_list[i];
-        let group = $('<div class="idol_group" style="border-left: solid 10px ' + colors[i] + ';"></div>');
-        for (let name of names) {
-            let icon_img = 'https://raw.githubusercontent.com/submeganep/submeganep.github.io/master/icon/2nd/' + name + '.png';
-            let icon = $('<div class="idol_icon" id="' + name + '"><a><img class="icon_image" src="' + icon_img + '" title="' + name + '"></a></div>');
-            group.append(icon);
-        }
-        $('#idol_container').append(group);
-        for (let name of names) {
-            $('#' + name).on('click', function() {
-                $('#picker').spectrum('set', '');
-                $('html, body').animate({scrollTop:$('#costume_container').offset().top});
-                $('#costume_container').empty();
-                displayIdolCostumes(name);
-            });
-        }
-
-    }
-
     
 });
