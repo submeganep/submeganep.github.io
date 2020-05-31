5844bcb65a3c94698d4c5f0efeaad9586e064bf6
submeganep <64298377+submeganep@users.noreply.github.com>
2020-05-31 09:43:09 +0900

    Add files via upload

---
+++ koshou/koshou.js
@@ -0,0 +1,6 @@
+$.getJSON('https://raw.githubusercontent.com/submeganep/submeganep.github.io/master/koshou/koshou.json', function (data) {
+    let keys = Object.keys(data);
+
+    console.log(keys);
+    
+});
