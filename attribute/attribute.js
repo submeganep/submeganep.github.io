df69e4daa24ca2d38da2c5104d29c07f4f0d12ec
submeganep <64298377+submeganep@users.noreply.github.com>
2020-08-05 23:41:53 +0900

    Add files via upload

--- attribute/attribute.js
+++ attribute/attribute.js
@@ -17,34 +17,30 @@ function getPosition(pr, fa, an) {
     return {x, y};
 }
 
-// アイドルごとの配置座標を算出
-for (let i = 0; i < idol_names.length; i++) {
-    let name = idol_names[i];
-    let pr = params['idol'][name]['Pr'];
-    let fa = params['idol'][name]['Fa'];
-    let an = params['idol'][name]['An'];
-    let {x, y} = getPosition(pr, fa, an);
-    params['idol'][name]['x'] = x;
-    params['idol'][name]['y'] = y;
-}
-
 // アイドル詳細表示
 function displayIdol(name, nodes, edges) {
 
-    // アイコンを拡大する
-    let x = params['idol'][name]['x'];
-    let y = params['idol'][name]['y'];    
+    // アイコンをリセット
     for (let i = 0; i < idol_names.length; i++) {
         nodes.update({
             id: idol_names[i],
             hidden: false,
         });
     }
+    nodes.remove('selected');
+
+    // アイドル名が空なら表示を隠して終了
+    if (name === '') {
+        $('$idol_container').hide();
+        $('$tunes_container').show();
+        return;
+    }
+
+    // アイコンを拡大
     nodes.update({
         id: name,
         hidden: true,
     });
-    nodes.remove('selected');
     nodes.add({
         id: 'selected',
         shape: 'image',
@@ -53,24 +49,17 @@ function displayIdol(name, nodes, edges) {
         mass: 10,
         shadow: true,
         fixed: true,
-        x: x,
-        y: y,
+        x: params['idol'][name]['x'],
+        y: params['idol'][name]['y'],
     });
 
     // アイドルのプロフィールを表示
-    $('#idol_container').empty();
     let idol = params['idol'][name];
+    $('#idol_eng').text(idol['Given'] + ' ' + idol['Family']);
+    $('#idol_name').css({'color': idol['カラーコード']}).text(name);
+    $('#idol_introduction').html(idol['ミリシタ紹介文'].replace(/\r?\n/g, '<br>'));
 
-    // アイドル名
-    $('#idol_container').append('<div class="idol_eng">' + idol['Given'] + ' ' + idol['Family'] + '</div>');
-    $('#idol_container').append('<div class="idol_name" style="color: ' + idol['カラーコード'] + ';">' + name + '</div>');
-
-    // 紹介文
-    let introduction = $('<div class="idol_introduction"></div>');
-    introduction.append('<div>' + idol['ミリシタ紹介文'].replace(/\r?\n/g, '<br>') + '</div>');
-    $('#idol_container').append(introduction);
-    
-    // 検索
+    // 表
     let name_niconico = name;
     let name_pixiv = name;
     if (name == 'ロコ') {
@@ -89,32 +78,27 @@ function displayIdol(name, nodes, edges) {
     search += '<a href="https://dic.nicovideo.jp/a/' + name_niconico + '" target="_blank">ニコニコ大百科</a>';
     search += '<br>';
     search += '<a href="https://dic.pixiv.net/a/' + name_pixiv + '" target="_blank">ピクシブ百科事典</a>';
-
-    // プロフィール
-    let table = $('<table></table>');
-    table.append('<tr><td>趣味</td><td>' + idol['趣味'] + '</td></tr>');
-    table.append('<tr><td>特技</td><td>' + idol['特技'] + '</td></tr>');
-    table.append('<tr><td>好きなもの</td><td>' + idol['好きなもの'] + '</td></tr>');
-    table.append('<tr><td>身長 / 体重</td><td>' + idol['身長'] + 'cm / ' + idol['体重'] + 'kg</td></tr>');
-    table.append('<tr><td>スリーサイズ</td><td>' + idol['B'] + '-' + idol['W'] + '-' + idol['H'] + '</td></tr>');
-    table.append('<tr><td>血液型</td><td>' + idol['血液型'] + '型</td></tr>');
-    table.append('<tr><td>利き手</td><td>' + idol['利き手'] + '</td></tr>');
-    table.append('<tr><td>出身地</td><td>' + idol['出身地'] + '</td></tr>');
-    table.append('<tr><td>誕生日</td><td>' + idol['誕生日'] + '</td></tr>');
-    table.append('<tr><td>年齢</td><td>' + idol['年齢'] + '歳</td></tr>');
-    table.append('<tr><td>属性</td><td>' + idol['PrFaAn'] + ' / ' + idol['VoDaVi'] + '</td></tr>');
-    table.append('<tr><td>CV</td><td>' + idol['CV'] + '</td></tr>');
-    table.append('<tr><td>検索</td><td>' + search + '</td></tr>');
-    $('#idol_container').append(table);
+    $('#趣味').text(idol['趣味']);
+    $('#特技').text(idol['特技']);
+    $('#好きなもの').text(idol['好きなもの']);
+    $('#身長体重').text(idol['身長'] + 'cm / ' + idol['体重'] + 'kg');
+    $('#スリーサイズ').text(idol['B'] + '-' + idol['W'] + '-' + idol['H']);
+    $('#血液型').text(idol['血液型'] + '型');
+    $('#利き手').text(idol['利き手']);
+    $('#出身地').text(idol['出身地']);
+    $('#誕生日').text(idol['誕生日']);
+    $('#年齢').text(idol['年齢'] + '歳');
+    $('#属性').text(idol['PrFaAn'] + ' / ' + idol['VoDaVi']);
+    $('#CV').text(idol['CV']);
+    $('#検索').html(search);
 
     // 背景
     // idol['ID']  // 03みたいなIDを背景右上に表示するとカッコいいかも
     $('#idol_container').css('background-image', 'url(./standing/' + name + '.png)');
 
     // 楽曲
-    $('#tunes_header').empty();
-    $('#tunes_header').append('<div class="container_header">▼ ' + name + 'の楽曲（楽曲名を選択して視聴）</div>')
-    $('#tunes_container').empty();
+    $('#tunes_items').empty();
+    $('#tunes_idol').text(name)
     for (let i = 0; i < tune_names.length; i++) {
         let tune_name = tune_names[i];
         let tune = tunes[tune_name];
@@ -122,58 +106,47 @@ function displayIdol(name, nodes, edges) {
             if (tune['idol_names'].length > 5) {
                 continue;
             }
-            let tune_container = $('<div class="tune_container"></div>');
-            tune_container.append('<div id="player_' + i + '"></div>');
-            tune_container.append('<div class="tune_name"><a id="tune_' + i + '">' + tune_name + '</a></div>');
-            tune_container.append('<div class="unit_name">' + tune['unit_name'] + '</div>');
-            let tune_idols = $('<div class="tune_idols"></div>');
+            let tune_body = $('<div class="col-lg-4 col-md-6 col-12 p-0 mb-2 mb-2 bg-light border border-white" />');
+            tune_body.append(`<div class="embed-responsive embed-responsive-16by9 bg-secondary text-white d-flex align-items-center justify-content-center" style="cursor: pointer;" id="player_${i}">YouTubeで試聴</div>`);
+            tune_body.append('<div class="text-center font-weight-bold">' + tune_name + '</div>');
+            let tune_idols = $('<div class="text-center"></div>');
             for (let idol_name of tune['idol_names']) {
                 tune_idols.append('<img src="./icon/' + idol_name + '.png" style="width: 60px;">');
             }
-            tune_container.append(tune_idols);
-            $('#tunes_container').append(tune_container);
-
-            $('#tune_' + i).on('click', function() {
-                if (!($('#player_' + i + ' iframe').length)) {
-                    let url = tune['url'];
-                    // let url = tune['url'].replace('http://', 'https://');
-                    $('#player_' + i).append('<iframe src="' + url + '" id="ytplayer" type="text/html" width="320" height="180" frameborder="0"></iframe>');
-                }
+            tune_body.append(tune_idols);
+            tune_body.append('<div class="text-center">' + tune['unit_name'] + '</div>');
+            $('#tunes_items').append(tune_body);
+            $('#player_' + i).on('click', function() {
+                $('#player_' + i).append('<iframe class="embed-responsive-item" src="' + tune['url'] + '" type="text/html"></iframe>');
             });
         }
     }
+
+    // 表示
+    $('#network_description').hide();
+    $('#idol_container').slideDown(400);
+    $('#tunes_container').slideDown(400);
 }
 
-// 診断
-function diagnose(nodes, edges) {
-
-    // スコアを算出
+// 予測
+function predict() {
     let pr = params['coef']['Pr'][questions.length];
     let fa = params['coef']['Fa'][questions.length];
     let an = params['coef']['An'][questions.length];
     for (let i = 0; i < questions.length; i++) {
-        if ($('#yes_' + i).hasClass('selected')) {
+        let answer = $('#radio_' + i + ' label.active input').val();
+        if (answer === 'yes') {
             pr += params['coef']['Pr'][i];
             fa += params['coef']['Fa'][i];
             an += params['coef']['An'][i];
         }
-        else if ($('#no_' + i).hasClass('selected')) {
+        else if (answer === 'no') {
             pr += params['coef']['Pr'][i + questions.length];
             fa += params['coef']['Fa'][i + questions.length];
             an += params['coef']['An'][i + questions.length];
         }
-        else if ($('#neutral_' + i).hasClass('selected')) {
+        else if (answer === 'na') {
         }
-        else {
-            return;
-        }
-    }
-    if (pr + fa + an == 0) {  // 全て「どちらでもない」場合
-        $('#result_container').empty();
-        $('#idol_container').empty();
-        $('#idol_container').css('background', '');
-        $('#tunes_header').text('');
-        return;
     }
     pr = 1 / (1 + Math.exp(-pr));
     fa = 1 / (1 + Math.exp(-fa));
@@ -182,43 +155,83 @@ function diagnose(nodes, edges) {
     pr /= sum;
     fa /= sum;
     an /= sum;
-    let {x, y} = getPosition(pr, fa, an);
+    return {pr, fa, an};
+}
 
-    // 属性
+// パーセント表示を更新
+function updatePercetage(pr, fa, an) {
+    let pr_percent = pr * 100;
+    let fa_percent = fa * 100;
+    let an_percent = an * 100;
+    $('#result_pr').text(Math.round(pr_percent));
+    $('#result_fa').text(Math.round(fa_percent));
+    $('#result_an').text(Math.round(an_percent));
+    $('#bar_pr').animate({'width': pr_percent + '%'});
+    $('#bar_fa').animate({'width': fa_percent + '%'});
+    $('#bar_an').animate({'width': an_percent + '%'});
+}
+
+// 結果を更新
+function updateResult(pr, fa, an) {
+
+    // 結果を更新
     let max_score = Math.max(pr, fa, an);
-    let result_summary = '';
-    let result_detail = '';
-    let result_percent = '';
-    if (pr == max_score){
-        result_summary = 'あたなはどこからどうみても<span class="attribute pr">Princess</span>です！';
+    let min_score = Math.min(pr, fa, an);
+    let result_summary;
+    let result_detail;
+    if (min_score === max_score) {
+        result_summary = 'あたなはどの属性とも言えないです';
+        result_detail = '「どちらでもない」ばかり選んでいませんか？';
+    }
+    else if (pr === max_score) {
+        result_summary = 'あたなはどこからどうみても <span class="attribute pr">Princess</span> です！';
         result_detail = '気持ちがまっすぐで周囲を巻き込んでいくパワーを持つあなたは、どうみてもプリンセスです。友達想いのあなたの周りにはいつも楽しい空気が流れているはず。もしかしたら自分では気付いていないかもしれませんが、あなたの一生懸命さにみんな勇気づけられていますよ！';
-    } else if (fa == max_score) {
-        result_summary = 'あなたは周囲の人から<span class="attribute fa">Fairy</span>だと思われています！';
+    }
+    else if (fa === max_score) {
+        result_summary = 'あなたは周囲の人から <span class="attribute fa">Fairy</span> だと思われています！';
         result_detail = '全身からかっこよさが溢れ、物事を深く考えがちなあなたは、普段からフェアリーだなと思われています。自他共に妥協を許さない姿勢はなにかと周りのレベルを引き上げているはず。落ち込むことも多いかもしれませんが、あなたの魅力に気付いている人はあなたが思うよりずっとたくさんいますよ！';
-    } else {
-        result_summary = 'あなたは<span class="attribute an">Angel</span>っぽいところがあるみたいですね～！';
+    }
+    else {
+        result_summary = 'あなたは <span class="attribute an">Angel</span> っぽいところがあるみたいですね～！';
         result_detail = '癒やしの空気を纏い自然体で生きるあなたは、エンジェルっぽいです！人と違うテンポで生きているあなたは、周りの人が越えられないハードルも簡単に飛び越えてみせているはず。きっと気にしていないとは思いますが、あなたの楽しく過ごす姿に心が安らいでいる人もいっぱいいますよ！';
     }
-    result_percent += '<span class="attribute pr">Princess ' + Math.round(pr * 100) + '%</span>';
-    result_percent += '<span class="attribute fa">Fairy ' + Math.round(fa * 100) + '%</span>';
-    result_percent += '<span class="attribute an">Angel ' + Math.round(an * 100) + '%</span>';
-    $('#result_container').empty();
-    $('#result_container').append('<div class="container_header">▼ 診断結果</div>')
-    $('#result_container').append('<div id="result_summary">' + result_summary + '</div>');
-    $('#result_container').append('<div id="result_percent">' + result_percent + '</div>');
-    $('#result_container').append('<div id="result_detail">' + result_detail + '</div>');
+    $('#result_summary').html(result_summary);
+    $('#result_detail').html(result_detail);
+    $('#result_container').slideDown(400);
 
     // 設問への回答が最も似ているアイドル
     let idol_similar = '';
-    let best_similarity = 0;
+    let best_similarity = -1;
     for (let i = 0; i < idol_names.length; i++) {
         let name = idol_names[i];
-        let answer = params['idol'][name]['answer'];
+
+        // max_scoreの属性と異なるアイドルはスキップ
+        if (min_score === max_score) {
+        }
+        else if (pr === max_score) {
+            if (params['idol'][name]['PrFaAn'] !== 'Pr') {
+                continue;
+            }
+        }
+        else if (fa === max_score) {
+            if (params['idol'][name]['PrFaAn'] !== 'Fa') {
+                continue;
+            }
+        }
+        else {
+            if (params['idol'][name]['PrFaAn'] !== 'An') {
+                continue;
+            }
+        }
+
+        let answers = params['idol'][name]['answer'];
         let similarity = 0;
-        for (let j = 0; j < answer.length; j++) {
-            if (answer[j] > 0 & $('#yes_' + j).hasClass('selected')) {
+        for (let j = 0; j < answers.length; j++) {
+            let answer = $('#radio_' + j + ' label.active input').val();
+            // XXX Noがきちんと与えられたらcosine類似度に変更する
+            if (answers[j] > 0 && answer === 'yes') {
                 similarity++;
-            } else if (answer[j] == 0 & $('#no_' + j).hasClass('selected')) {
+            } else if (answers[j] < 0 && answer === 'no') {
                 similarity++;
             }
         }
@@ -229,58 +242,43 @@ function diagnose(nodes, edges) {
     }
 
     // 座標が最も近いアイドル
-    let idol_nearest = '';
-    let best_dist = 2.0;
-    for (let i = 0; i < idol_names.length; i++) {
-        let name = idol_names[i];
-        let pr_diff = pr - params['idol'][name]['Pr'];
-        let fa_diff = fa - params['idol'][name]['Fa'];
-        let an_diff = an - params['idol'][name]['An'];
-        let dist = pr_diff ** 2 + fa_diff ** 2 + an_diff ** 2;
-        if (dist < best_dist) {
-            best_dist = dist;
-            idol_nearest = name;
-        }
-    }
-    $('#result_container').append('<div id="result_idol">似ているアイドルは <a id="link_' + idol_similar + '">' + idol_similar + '</a> です</div>');
-    $('#link_' + idol_similar).on('click', function() {
-        displayIdol(idol_similar, nodes, edges);
-    });
+    // let idol_nearest = '';
+    // let best_dist = 2.0;
+    // for (let i = 0; i < idol_names.length; i++) {
+    //     let name = idol_names[i];
+    //     let pr_diff = pr - params['idol'][name]['Pr'];
+    //     let fa_diff = fa - params['idol'][name]['Fa'];
+    //     let an_diff = an - params['idol'][name]['An'];
+    //     let dist = pr_diff ** 2 + fa_diff ** 2 + an_diff ** 2;
+    //     if (dist < best_dist) {
+    //         best_dist = dist;
+    //         idol_nearest = name;
+    //     }
+    // }
 
-    // アイドルのプロフィールを表示
-    displayIdol(idol_similar, nodes, edges);
+    // $('#result_idol').html('そんなあなたに似たアイドルは… ' + idol_similar);
+    $('#result_idol').html('そんなあなたに似たアイドルは…');
 
-    // 星を表示
-    nodes.update({
-        id: 'you',
-        x: x,
-        y: y,
-    });
-
+    return idol_similar;
 }
 
-function click_button(i) {
-    $('#yes_' + i).removeClass('selected');
-    $('#no_' + i).removeClass('selected');
-    $('#neutral_' + i).removeClass('selected');
-    let next = $('#q_' + (i + 1));
-    // 次の設問を開く
-    if (next.is(':hidden')) {
-        next.slideDown(400);
-        $('html,body').animate({scrollTop: next.offset().top}, 400);
-        $('#progress_bar').val(i + 2);
-        $('#progress_text').text((i + 2) + ' / ' + questions.length);
-    }
-    // 全ての設問に回答
-    if (i + 1 == questions.length) {
-        $('#progress_container').hide();
-        $('html,body').animate({scrollTop: $('#result_container').offset().top}, 400);
-        // $('#network').show();
-    }
+// 似たアイドルを取得
+function getSimilarIdol(pr, fa, an) {
 }
 
 // HTMLの読み込みが全て完了した後に実行
 $(function(){
+
+    // アイドルごとの配置座標を算出
+    for (let i = 0; i < idol_names.length; i++) {
+        let name = idol_names[i];
+        let pr = params['idol'][name]['Pr'];
+        let fa = params['idol'][name]['Fa'];
+        let an = params['idol'][name]['An'];
+        let {x, y} = getPosition(pr, fa, an);
+        params['idol'][name]['x'] = x;
+        params['idol'][name]['y'] = y;
+    }
 
     // ノードとエッジ
     let nodes = [];
@@ -429,42 +427,60 @@ $(function(){
 
     // 設問
     for (let i = 0; i < questions.length; i++) {
-        let q = $('<div class="question_container" id="q_' + i + '"></div>');
-        q.append('<div class="question_text">Q' + (i + 1) + '. ' + questions[i] + '</div>');
-        let button = $('<div class="button_container"></div>');
-        button.append('<div class="answer_button" id="yes_' + i + '">はい</div>');
-        button.append('<div class="answer_button" id="neutral_' + i + '">どちらでもない</div>');
-        button.append('<div class="answer_button" id="no_' + i + '">いいえ</div>');
-        q.append(button);
-        $('#questions_container').append(q);
-        // クリック
-        $('#yes_' + i).on('click', function () {
-            click_button(i);
-            $('#yes_' + i).addClass('selected');
-            diagnose(nodes, edges);
-        });
-        $('#neutral_' + i).on('click', function () {
-            click_button(i);
-            $('#neutral_' + i).addClass('selected');
-            diagnose(nodes, edges);
-        });
-        $('#no_' + i).on('click', function () {
-            click_button(i);
-            $('#no_' + i).addClass('selected');
-            diagnose(nodes, edges);
+
+        // カル―セル
+        let question = `<div class="pt-2 pb-2">Q${i + 1}. ${questions[i]}</div>`;
+        let answer = $(`<div class="btn-group btn-group-toggle" data-toggle="buttons" id="radio_${i}" />`);
+        answer.append(`<label class="btn btn-outline-dark"><input type="radio" value="yes">はい</label>`);
+        answer.append(`<label class="btn btn-outline-dark"><input type="radio" value="na">どちらでもない</label>`);
+        answer.append(`<label class="btn btn-outline-dark"><input type="radio" value="no">いいえ</label>`);
+        let q = $('<div class="carousel-item text-center"></div>');
+        q.append(question);
+        q.append(answer);
+        $('#questions_container .carousel-inner').append(q);
+        $('#questions_container .carousel-indicators').append(`<li data-target="#questions_container" data-slide-to="${i + 1}"></li>`);
+
+        // 回答クリック
+        $('#radio_' + i + ' input').on('click', function() {
+
+            // 次の設問へ
+            $('#questions_container').carousel('next');
+
+            // 予測
+            let {pr, fa, an} = predict();
+
+            // パーセントを更新
+            updatePercetage(pr, fa, an);
+
+            // 星の位置を更新
+            let {x, y} = getPosition(pr, fa, an);
+            nodes.update({
+                id: 'you',
+                x: x,
+                y: y,
+            });
+
+            // 最後の設問が終わったら結果へ移動
+            if (i + 1 === questions.length) {
+                // 結果を表示
+                let similar_idol = updateResult(pr, fa, an);
+                // 似たアイドルを表示
+                displayIdol(similar_idol, nodes, edges);
+                // スクロール
+                $('html,body').animate({scrollTop: $('#attr_container').offset().top}, 400);
+            }
+            // 一度診断した後は回答を変更するごとに結果を変更
+            else if ($('#result_container').is(':visible')) {
+                // 結果を表示
+                let similar_idol = updateResult(pr, fa, an);
+                // 似たアイドルを表示
+                displayIdol(similar_idol, nodes, edges);
+            }
         });
     }
 
-    // 開始ボタン
+    // 回答開始
     $('#start_button').on('click', function () {
-        $('#start_button').hide();
-        $('#q_0').slideDown(400);
-        $('html,body').animate({scrollTop: $('#q_0').offset().top}, 400);
-
-        
-        $('#progress_container').append('<div id="progress_text">1 / ' + questions.length + '</div>');
-        $('#progress_container').append('<progress id="progress_bar" min="0" max="' + questions.length + '" value="1"></progress>');
+        $('#questions_container').carousel('next');
     });
-
-    
 });
