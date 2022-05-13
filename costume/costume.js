ead17379daa0094da692e1055628ff9ced7e930c
submeganep <64298377+submeganep@users.noreply.github.com>
2022-05-14 01:23:36 +0900

    Add files via upload

--- costume/costume.js
+++ costume/costume.js
@@ -568,7 +568,7 @@ $(function(){
         $('#type_pst').prop('checked', false);
         $('#type_sr').prop('checked', false);
         $('#date_begin').val('2017-06-29');
-        $('#date_end').val('2020-05-18');
+        $('#date_end').val('2021-04-11');
         displayCostumes();
     });
     $('#button_ticket1').on('click', function() {
@@ -584,7 +584,7 @@ $(function(){
         $('#type_pst').prop('checked', false);
         $('#type_sr').prop('checked', false);
         $('#date_begin').val('2017-06-29');
-        $('#date_end').val('2021-09-30');
+        $('#date_end').val('2022-01-18');
         displayCostumes();
     });
     $('#button_ticket2').on('click', function() {
@@ -616,7 +616,7 @@ $(function(){
         $('#type_pst').prop('checked', true);
         $('#type_sr').prop('checked', false);
         $('#date_begin').val('2017-06-29');
-        $('#date_end').val('2021-03-19');
+        $('#date_end').val('2021-10-18');
         displayCostumes();
     });
     $('#button_reset').on('click', function() {
